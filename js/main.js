24622dea506760240ea86f078c263569375eb292
Jasen Green <jasen.green@protonmail.com>
2018-10-30 15:07:25 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-document.getElementById("btn").addEventListener("click", run);
+document.getElementById("btn").addEventListener("click", request);
 
-// I suggest you use a more meaningful name instead of run. Try to be descriptive.
-// What will this function do? Call it that.
-function run() {
+function request() {
   let data = document.getElementById("url").value;
 
   fetch("http://localhost:3000", {
@@ -16,7 +14,6 @@ function run() {
       return response.json();
     })
     .then(function(data) {
-      // remove space here.
       document.getElementById("json").innerHTML = JSON.stringify(data);
     });
-}
+};
